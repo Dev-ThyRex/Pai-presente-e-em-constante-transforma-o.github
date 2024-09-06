ab11300fe78aa0cfd4fd85fd68479a7ea08894c5
Thyago R Reis <pr.thyagor.reis@gmail.com>
2024-09-06 13:49:32 -0300

    Imersão Dev Google Gemini
    Aula 4 e aula extra ok.

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo : "Paizada",
+    descricao : "Ser pai é uma jornada repleta de desafios e recompensas. É assumir o compromisso de criar, educar e proteger um novo ser humano, moldando seu caráter e oferecendo um ambiente seguro e amoroso para seu desenvolvimento. A paternidade exige dedicação, paciência e uma constante busca por aprendizado, pois cada filho é único e apresenta suas próprias necessidades. É um privilégio acompanhar o crescimento e as conquistas de um filho, mas também uma responsabilidade imensa de prepará-lo para enfrentar os desafios da vida.",
+    Link : "https://paizinhovirgula.com/livro-hold-on-to-your-kids-pais-ocupados-filhos-distantes/"
+},
+{
+    titulo : "Balaio de Pais",
+    descricao : "Podcast sobre a paternidade real, com histórias e dicas para pais de todas as idades.",
+},
+{
+    titulo : "AfroPai",
+    descricao : "Podcast que aborda a paternidade negra, com reflexões sobre raça, cultura e identidade.",
+},
+{
+    titulo : "Ôpaiê",
+    descricao : "Podcast com bate-papos sobre paternidade, maternidade e família.", 
+}
+];
